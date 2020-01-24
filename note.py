4366f3d395e2c5640c986870364417fb2d3ffa64
MajorThird <test@testtesttest.org>
2020-01-25 00:59:48 +0100

    added track variable

--- note.py
+++ note.py
@@ -1,11 +1,12 @@
 class Note(object):
-    def __init__(self, velocity=127, pitch=50, start_ticks=0, syllable="a"):
+    def __init__(self, velocity=127, pitch=50, start_ticks=0, track=0, syllable="a"):
         self.velocity = velocity
         self.pitch = pitch
         self.start_ticks = start_ticks
+        self.track = track
         self.syllable = syllable
 
     def __str__(self):
-        out_string = "Note velocity=%i pitch=%i start_ticks=%i syllable=%s" % (
-            self.velocity, self.pitch, self.start_ticks, self.syllable)
+        out_string = "Note velocity=%i pitch=%i start_ticks=%i track=%i syllable=%s" % (
+            self.velocity, self.pitch, self.start_ticks, self.track, self.syllable)
         return out_string
