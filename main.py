8ba34a5df0c4a43e1a9d84ef9d742bf7083d2a6c
MajorThird <test@testtesttest.org>
2020-01-24 23:58:56 +0100

    do some tests with midi

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 import configparser
 import argparse
+import midi
+
+
+
+def is_note_off(event):
+    velocity = event.data[1]
+    name = event.name
+    return name == "Note Off" or velocity == 0
+
+
+def read_midi(filename):
+    midi_tracks = midi.read_midifile(filename)
+    note_ons = []
+    note_offs = []
+    for t in midi_tracks:
+        total_ticks = 0
+        for elem in t:
+            total_ticks += elem.tick
+            if elem.name in ["Note On", "Note Off"]:
+                if is_note_off(elem):
+                    note_offs.append((total_ticks, elem))
+                else:
+                    note_ons.append((total_ticks, elem))
+    note_ons = sorted(note_ons, key=lambda x: x[0])
+    note_offs = sorted(note_offs, key=lambda x: x[0])
+    return note_ons, note_offs
+
+
+def midi_tests(config):
+    filename = config["DEFAULT"]["PathToMidiFileHuman"]
+    ons, offs = read_midi(filename)
+
+
 
 def get_config(filename):
     config = configparser.ConfigParser()
@@ -14,8 +47,8 @@ def main():
 
     filename = arguments["config"]
     config = get_config(filename)
-    print(config["DEFAULT"]["PathToMidiFileHuman"])
 
+    midi_tests(config)
 
 
 if __name__ == '__main__':
