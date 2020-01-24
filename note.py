f99cbc485a67d4a96d3556c13e2d1bd623ff6380
MajorThird <test@testtesttest.org>
2020-01-25 00:40:34 +0100

    changed parameter order

--- note.py
+++ note.py
@@ -1,6 +1,6 @@
 class Note(object):
-    def __init__(self, syllable="a", velocity=127, pitch=50, start_ticks=0):
-        self.syllable = syllable
+    def __init__(self, velocity=127, pitch=50, start_ticks=0, syllable="a"):
         self.velocity = velocity
         self.pitch = pitch
         self.start_ticks = start_ticks
+        self.syllable = syllable
