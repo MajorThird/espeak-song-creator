7eca92d233b325dd4b2b9a97f80e6598e60d6910
MajorThird <test@testtesttest.org>
2020-01-24 23:28:20 +0100

    implement config file and command line arguments

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import configparser
+import argparse
+
+def get_config(filename):
+    config = configparser.ConfigParser()
+    config.read(filename)
+    return config
+
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", required=False, default="options.cfg", help="path to program options file")
+    arguments = vars(parser.parse_args())
+
+    filename = arguments["config"]
+    config = get_config(filename)
+    print(config["DEFAULT"]["PathToMidiFileHuman"])
+
+
 
 if __name__ == '__main__':
     main()
