06479c8cad4937db40214f8950fe113cf8ebeaa2
MajorThird <test@testtesttest.org>
2020-01-25 02:02:24 +0100

    add note ends

--- note.py
+++ note.py
@@ -1,13 +1,15 @@
 class Note(object):
-    def __init__(self, velocity=127, pitch=50, start_ticks=0, start_time=0.0, track=0, syllable="a"):
+    def __init__(self, velocity=127, pitch=50, start_ticks=0, end_ticks=0, start_time=0.0, end_time=0.0, track=0, syllable="a"):
         self.velocity = velocity
         self.pitch = pitch
         self.start_ticks = start_ticks
+        self.end_ticks = end_ticks
         self.start_time = start_time
+        self.end_time = end_time
         self.track = track
         self.syllable = syllable
 
     def __str__(self):
-        out_string = "Note velocity=%i pitch=%i start_ticks=%i start_time=%.3f track=%i syllable=%s" % (
-            self.velocity, self.pitch, self.start_ticks, self.start_time, self.track, self.syllable)
+        out_string = "Note velocity=%i pitch=%i start_ticks=%i end_ticks=%i start_time=%.3f end_time=%.3f track=%i syllable=%s" % (
+            self.velocity, self.pitch, self.start_ticks, self.end_ticks, self.start_time, self.end_time, self.track, self.syllable)
         return out_string
