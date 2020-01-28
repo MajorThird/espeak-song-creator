cf9d1d570dc7498103a941c0edca7b65e3c108ea
MajorThird <test@testtesttest.org>
2020-01-28 21:00:19 +0100

    move stuff from main to here

--- note.py
+++ note.py
@@ -17,7 +17,18 @@ class Note(object):
         self.phoneme = phoneme
         self.finished = True
 
+    def calculate_start_and_end_time(self, tempo_bpm, resolution):
+        self.start_time = get_time_of_ticks(self.start_ticks, resolution, tempo_bpm)
+        self.end_time = get_time_of_ticks(self.end_ticks, resolution, tempo_bpm)
+
     def __str__(self):
         out_string = "Note velocity=%i pitch=%i start_ticks=%i end_ticks=%i start_time=%.3f end_time=%.3f phoneme=%s" % (
             self.velocity, self.pitch, self.start_ticks, self.end_ticks, self.start_time, self.end_time, self.phoneme)
         return out_string
+
+
+
+def get_time_of_ticks(ticks, resolution, tempo_bpm):
+    time_per_beat = 60.0 / tempo_bpm
+    time_per_tick = time_per_beat / resolution
+    return time_per_tick * ticks
