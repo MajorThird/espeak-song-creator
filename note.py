e5bfa2982db342fe72b635345a0879d49c9bc900
MajorThird <test@testtesttest.org>
2020-01-28 20:55:15 +0100

    remove unused stuff

--- note.py
+++ note.py
@@ -7,7 +7,6 @@ class Note(object):
             end_ticks=0,
             start_time=0.0,
             end_time=0.0,
-            track=0,
             phoneme="a"):
         self.velocity = velocity
         self.pitch = pitch
@@ -15,11 +14,10 @@ class Note(object):
         self.end_ticks = end_ticks
         self.start_time = start_time
         self.end_time = end_time
-        self.track = track
         self.phoneme = phoneme
         self.finished = True
 
     def __str__(self):
-        out_string = "Note velocity=%i pitch=%i start_ticks=%i end_ticks=%i start_time=%.3f end_time=%.3f track=%i phoneme=%s" % (
-            self.velocity, self.pitch, self.start_ticks, self.end_ticks, self.start_time, self.end_time, self.track, self.phoneme)
+        out_string = "Note velocity=%i pitch=%i start_ticks=%i end_ticks=%i start_time=%.3f end_time=%.3f phoneme=%s" % (
+            self.velocity, self.pitch, self.start_ticks, self.end_ticks, self.start_time, self.end_time, self.phoneme)
         return out_string
